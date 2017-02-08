ffbbfe07a28cc8e83e55a079b3eccfa117ecf7be
Tom Scott <tomscott85@gmail.com>
2017-02-08 20:52:10 +1100

    angular code

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,20 @@
-// js code for client 
+angular.module('TestApp', []);  // sets new module which is in <head> tag in index.ejs - array is dependent modules
+
+angular.module('TestApp')
+  .controller('MainController', ctrlFunc);
+
+  function ctrlFunc() {
+    this.message = 'Hello';
+
+    this.people = [
+      {
+        name: 'John'
+      },
+      {
+        name: 'Jane'
+      },
+      {
+        name: 'Jim'
+      }
+    ]
+  }
